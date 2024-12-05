a70a63142de576e519a59041aa25e0d3c9f6ac37
Civan Rıdar Öztekin <57915604+CROztk@users.noreply.github.com>
2024-12-05 17:59:53 +0100

    Player Animations added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,19 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Rigidbody2D)), RequireComponent(typeof(Animator))]
 public class PlayerController : MonoBehaviour
 {
     public float walkSpeed = 5f;
+    public float runSpeed = 8f;
     Vector2 moveInput;
 
-    public bool IsMoving { get; private set; }
+    public float speed => IsRunning ? runSpeed : walkSpeed;
+
+    [SerializeField]
+    private bool _isMoving = false;
+    public bool IsMoving { get 
+    {
+        return _isMoving;
+    } private set
+    {
+        _isMoving = value;
+        animator.SetBool(AnimationStrings.isMoving, value);
+    } }
+
+    [SerializeField]
+    private bool _isRunning = false;
+    public bool IsRunning { get 
+    {
+        return _isRunning;
+    } private set
+    {
+        _isRunning = value;
+        animator.SetBool(AnimationStrings.isRunning, value);
+    } }
 
     Rigidbody2D rb;
+    Animator animator;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Start is called before the first frame update
@@ -32,7 +57,7 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(moveInput.x * walkSpeed, rb.velocity.y);
+        rb.velocity = new Vector2(moveInput.x * speed, rb.velocity.y);
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -40,6 +65,32 @@ public class PlayerController : MonoBehaviour
         moveInput = context.ReadValue<Vector2>();
         
         IsMoving = moveInput != Vector2.zero;
+
+        SetFacingDirection();
         
+    }
+
+    private void SetFacingDirection()
+    {
+        if(moveInput.x > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if(moveInput.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+    }
+
+    public void OnRun(InputAction.CallbackContext context)
+    {
+        if(context.started)
+        {
+            IsRunning = true;
+        }
+        else if(context.canceled)
+        {
+            IsRunning = false;
+        }
     }
 }
