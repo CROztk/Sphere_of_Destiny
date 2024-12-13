77545b974c5a62807c572850c3a3ff539d743c76
iremcnar <41iremcinar@gmail.com>
2024-12-13 16:02:17 +0300

    enemy AI states

--- Assets/Scripts/AnimationStrings.cs
+++ Assets/Scripts/AnimationStrings.cs
@@ -12,4 +12,6 @@ internal class AnimationStrings
     internal static string isAlive = "isAlive";
     internal static string hitTrigger = "hit"; 
     internal static string lockVelocity = "lockVelocity";
+    internal static string hasTarget = "hasTarget";
+    
 }
