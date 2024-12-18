a8f9f1e812bb7bc96613adb4bdc4c35f2a113222
iremcnar <41iremcinar@gmail.com>
2024-12-19 01:28:23 +0300

    multiple attacks combo for player

--- Assets/Scripts/Knight.cs
+++ Assets/Scripts/Knight.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D), typeof(TouchingDirections)), RequireComponent(typeof(Animator)), RequireComponent(typeof(Damageable))]
 public class Knight : MonoBehaviour
 {
-    public float walkSpeed = 3f;
+    public float walkAcceleration = 3f;
+    public float maxSpeed = 3f;
     public float walkStopRate= 0.05f;
     public DetectionZone attackZone;
     public DetectionZone cliffDetectionZone;
@@ -95,10 +96,14 @@ public class Knight : MonoBehaviour
         }
         if(!damageable.LockVelocity)
         {
-            if(CanMove) 
-                rb.velocity = new Vector2(walkSpeed * walkDirectionVector.x, rb.velocity.y);
+            if (CanMove)
+
+                rb.velocity = new Vector2(
+                    Mathf.Clamp(rb.velocity.x + (walkAcceleration * walkDirectionVector.x * Time.fixedDeltaTime),
+                        -maxSpeed, maxSpeed),
+                    rb.velocity.y);
             else
-                rb.velocity = new Vector2(Mathf.Lerp(rb.velocity.x,0,walkStopRate), rb.velocity.y);
+                rb.velocity = new Vector2(Mathf.Lerp(rb.velocity.x, 0, walkStopRate), rb.velocity.y);
         }
         
         
