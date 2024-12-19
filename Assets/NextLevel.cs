92a58a919a09253ca67ee5c34a87602dfa357685
Civan Rıdar Öztekin <57915604+CROztk@users.noreply.github.com>
2024-12-19 18:06:15 +0100

    Next level beer animations

--- Assets/NextLevel.cs
+++ Assets/NextLevel.cs
@@ -4,13 +4,33 @@ using UnityEngine;
 
 public class NextLevel : MonoBehaviour
 {
+    // animator
+    private Animator animator;
+
+    private void Awake()
+    {
+        // get the animator
+        animator = GetComponent<Animator>();
+    }
     void OnTriggerEnter2D(Collider2D other)
     {
+        
+        if (other.CompareTag("Player"))
+        {
+            animator.SetTrigger("PlayerTriggered");
+        }
+    }
+
+    public void LoadNextLevel()
+    {
         bool hasNextLevel = UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex + 1 < UnityEngine.SceneManagement.SceneManager.sceneCountInBuildSettings;
-        if (other.CompareTag("Player") && hasNextLevel)
+        if (hasNextLevel)
         {
             // Load the next level
             UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex + 1);
+            Debug.Log("Next Level");
         }
     }
+
+    
 }
